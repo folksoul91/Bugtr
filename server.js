9c15cd4f78818cdb1670799c557fac889b0e770a
Dane Kim <mr.dane@danes-mbp.lan>
2022-05-18 19:56:35 -0400

    creating new and create routes. Also adding a body middleware.

--- server.js
+++ server.js
@@ -6,14 +6,24 @@ const PORT = 3000;
 const budget = require("./models/budget");
 
 //middleware
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 app.get("/budget", (req, res) => {
   res.render("index.ejs", { allBudgets: budget });
 });
 
+app.get("/budget/new", (req, res) => {
+  res.render("new.ejs");
+});
+
 app.get("/budget/:indexOfBugetArray", (req, res) => {
   res.render("show.ejs", { budgets: budget[req.params.indexOfBugetArray] });
+});
+
+app.post("budget", (req, res) => {
+  budget.push(req.body);
+  res.redirect("/budget");
 });
 
 app.listen(PORT, () => {
