1cd6e61735ae40f1a0989e1162a33feb3a969f99
Dane Kim <mr.dane@danes-mbp.lan>
2022-05-18 14:55:56 -0400

    mounting budget route to say hello

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/buget", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log("Listening on PORT:", PORT);
+});
